dc7793e150a1e91f763486d8650edad0cff42505
nkuchenov-hash <nkuchenov@gmail.com>
2026-07-27 10:51:58 -0400

    Add runtime integration configuration

---
+++ runtime-config.js
@@ -0,0 +1,12 @@
+export const runtimeConfig = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  googleMapsBrowserKey: "",
+  functionsRegion: "europe-west1"
+};
